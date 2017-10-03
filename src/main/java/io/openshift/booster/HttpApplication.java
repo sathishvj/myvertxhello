87b140505a8ec823d51d771374f2d6e3b1bab210
sathishvj <sathishvj@yahoo.com>
2017-10-03 10:28:20 +0000

    Reset back to world.

--- src/main/java/io/openshift/booster/HttpApplication.java
+++ src/main/java/io/openshift/booster/HttpApplication.java
@@ -39,7 +39,7 @@ public class HttpApplication extends AbstractVerticle {
   private void greeting(RoutingContext rc) {
     String name = rc.request().getParam("name");
     if (name == null) {
-      name = "John";
+      name = "World";
     }
 
     JsonObject response = new JsonObject()
